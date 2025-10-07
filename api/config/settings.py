fde1402d85f5e0f0acd2fcf0ccc96b6c6ae3abd0
Shuwen Wang <shuwen.wang@talentneuron.com>
2025-10-07 19:18:15 -0400

    set up cicd

--- api/config/settings.py
+++ api/config/settings.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     model_size: str = os.getenv("MODEL_SIZE", "small")
     compute_type: str = os.getenv("COMPUTE_TYPE", "int8")
     max_file_mb: int = int(os.getenv("MAX_FILE_MB", "30"))
-    max_duration_sec: int = int(os.getenv("MAX_DURATION_SEC", "600"))
+    max_duration_sec: int = int(os.getenv("MAX_DURATION_SEC", "3600"))
     host: str = os.getenv("HOST", "0.0.0.0")
     port: int = int(os.getenv("PORT", "8000"))
 
